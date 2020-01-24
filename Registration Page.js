f72c29da613a60981f361d5481d6b50855da6eea
VishnuSastryHK <42547521+VishnuSastryHK@users.noreply.github.com>
2020-01-24 17:47:58 +0530

    Update Registration Page.js

--- Registration Page.js
+++ Registration Page.js
@@ -1,112 +1,102 @@
-function printError(elemId, hintMsg) 
-{
-    document.getElementById(elemId).innerHTML = hintMsg;
+function printError(elementId, hintMsg) {
+    document.getElementById(elementId).innerHTML = hintMsg;
 }
 
-function validate1()
-{ 
-
-  var name,email,pwd,cpwd;
-  var boolNameErr = boolEmailErr = boolPassErr = boolCPassErr = boolMobileErr = true;
-  
-  name = document.getElementsById("NAME").value;
-  email = document.getElementById("email").value;
-  pwd = document.getElementsByName("pwd").value;
-  cpwd = document.getElementsByName("cpwd").value;
-
-
-// Validate name
-if(name=="") 
+function validation()
 {
-    //document.getElementById("nameErr").innerHTML="jnjk";
-    printError("nameErr", "Please enter your name");
-} 
-else 
-{
-    var nameRegex = /^[a-zA-Z\s]+$/;                
-    if(nameRegex.test(name)==false) 
+
+    var name, email, pwd, cpwd;
+    var boolNameErr = boolEmailErr = boolPassErr = boolCPassErr = boolMobileErr = true;
+
+    name = document.getElementById("NAME").value;
+    email = document.getElementById("EMAIL").value;
+    pwd = document.getElementById("PWD").value;
+    cpwd = document.getElementById("CPWD").value;
+
+
+    // Validate name
+    if (name == "") 
     {
-        printError("nameErrID", "Please enter a valid name");
-    } 
+        //document.getElementById("nameErr").innerHTML="jnjk";
+        printError("nameErrID", "* Please enter your name");
+    }
     else 
     {
-        printError("nameErr", "");
-        boolNameErr = false;
-    }
-}
-  // Regular expression for basic email validation
-/*  var emailRegex=/^\S+@\S+\.\S+$/;
-  
-  if(email == "") 
-  {
-
-    printError("emailErr", "Please enter your email address");
-} 
-else 
-{
-    
-    if(emailRegex.test(email) == false) 
-    {    printError("emailErr", "Please enter your email address");
-
-            } 
-    else
-    {
-        printError("emailErr", "");
-        emailErr = false;
-    }
-}
-
-if(pwd=="")
-{
-    printError("passErr", "Please enter your password");
-}
-else
-    {
-        printError("passErr", "");
-        passErr = false;
-    }
-
-    if(cpwd=="")
-    {
-        printError("cpassErr", "Please Re-enter your password");
-    }
-    else
+        var nameRegex = /^[a-zA-Z\s]+$/;
+        if (nameRegex.test(name) == false) 
         {
-            printError("cpassErr", "");
-            passErr = false;
+            printError("nameErrID", "* Please enter a valid name");
         }
+        else 
+        {
 
-        
-        
-if(pwd!=cpwd)
-{
-    printError("cpassErr", "Passwords do not match");
-
-}
-else
-    {
-        printError("cpassErr", "");
-        cpassErr = false;
+            printError("nameErrID", "");
+            boolNameErr = false;
+        }
     }
-*/
+
+    // Regular expression for basic email validation
+    var emailRegex = /^\S+@\S+\.\S+$/;
+
+    if (email == "") 
+    {
+        printError("emailErrID", "* Please enter your email address");
+    }
+    else 
+    {
+
+        if (emailRegex.test(email) == false) 
+        {
+            printError("emailErrID", "* Please enter a valid email address");
+        }
+        else 
+        {
+            printError("emailErrID", "");
+            boolEmailErr = false;
+        }
+    }
+
+    if (pwd == "") 
+    {
+        printError("passErrID", "* Please enter your password");
+    }
+    else 
+    {
+        printError("passErrID", "");
+        boolPassErr = false;
+    }
+
+    if (cpwd == "") 
+    {
+        printError("cpErrID","* Please Re-Enter your password");
+    }
+    else 
+    {
+        printError("cpErrID", "");
+        boolCPpassErr = false;
+    }
 
 
-/*   // Prevent the form from being submitted if there are any errors
-if((nameErr || emailErr ||  passErr||cpassErr||mobileErr ) == true) 
-{
-    validate();
-    return false;
-} 
-else 
-{
-     // Creating a string from input data for preview
-     var dataPreview = "You've entered the following details: \n" +
-                       "Full Name: " + name + "\n" +
-                       "Email Address: " + email + "\n" +
-     // Display input data in a dialog box before submitting the form
-     alert(dataPreview);
- }*/
- 
+    if (pwd != cpwd) 
+    {
+        printError("cpErrID", "* Passwords do not match");
+    }
+    
+
+
+
+    // Prevent the form from being submitted if there are any errors
+    if ((boolNameErr || boolEmailErr || boolPassErr || boolCPassErr || boolMobileErr) == false) 
+    {
+        // Creating a string from input data for preview
+        var dataPreview = "You've entered the following details: \n" +
+            "Full Name: " + name + "\n" +
+            "Email Address: " + email + "\n" +
+            // Display input data in a dialog box before submitting the form
+            alert(dataPreview);
+    }
+
 }
+  
 
 
